5052259a8c88cce739a9fa009c06060784c61773
Hwan <skyi0909@naver.com>
2023-10-18 02:51:34 +0900

    Timestamped를 course에 상속, 생성,수정시간 적용을 위한 어노테이션 추가

--- CodingStudy_sh/SpringBasic-sparta/week02/src/main/java/com/sparta/week02/Week02Application.java
+++ CodingStudy_sh/SpringBasic-sparta/week02/src/main/java/com/sparta/week02/Week02Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+@EnableJpaAuditing //이 어노테이션으로 수정일자가 저장이됨
 @SpringBootApplication
 public class Week02Application {
 
