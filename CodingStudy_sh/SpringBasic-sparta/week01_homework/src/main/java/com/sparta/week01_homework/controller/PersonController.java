a4b42b7459f295f58808e46a239c7eee7246967c
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-11-01 18:36:32 +0900

    crud 관련 내용

--- CodingStudy_sh/SpringBasic-sparta/week01_homework/src/main/java/com/sparta/week01_homework/controller/PersonController.java
+++ CodingStudy_sh/SpringBasic-sparta/week01_homework/src/main/java/com/sparta/week01_homework/controller/PersonController.java
@@ -1,19 +1,52 @@
 package com.sparta.week01_homework.controller;
 
+import com.sparta.week01_homework.domain.PersonRepository;
+import com.sparta.week01_homework.domain.PersonRequestDto;
 import com.sparta.week01_homework.models.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.week01_homework.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class PersonController {
+    private final PersonRepository personRepository;
+    private final PersonService personService;
+
     @GetMapping("/myinfo")
     public Person getPersonInfo() {
-    Person person = new Person();
-    person.setName("손흥민");
-    person.setAddress("런던");
-    person.setAge(28);
-    person.setJob("대한민국 축구선수");
+        Person person = new Person();
+        person.setName("손흥민");
+        person.setAddress("런던");
+        person.setAge(28);
+        person.setJob("대한민국 축구선수");
 
-    return person;
+        return person;
     }
+
+    @PostMapping("/api/persons")
+    public Person createPerson(@RequestBody PersonRequestDto requestDto) {
+        Person person = new Person(requestDto);
+        return personRepository.save(person);
+    }
+
+    @GetMapping("/api/persons")
+    public List<Person> getPersons() {
+        return personRepository.findAll();
+    }
+
+    @PutMapping("/api/persons/{id}")
+    public Long updatePersons(@PathVariable Long id, @RequestBody PersonRequestDto requestDto) {
+        return personService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/persons/{id}")
+    public Long deletePersons(@PathVariable Long id) {
+        personRepository.deleteById(id);
+        return id;
+    }
+
+
 }
--- CodingStudy_sh/SpringBasic-sparta/week01_homework/src/main/java/com/sparta/week01_homework/models/Person.java
+++ CodingStudy_sh/SpringBasic-sparta/week01_homework/src/main/java/com/sparta/week01_homework/models/Person.java
@@ -1,7 +1,25 @@
 package com.sparta.week01_homework.models;
 
+import com.sparta.week01_homework.domain.PersonRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false) //꼭잇어야함
     private String name;
+    @Column(nullable = false) //꼭잇어야함
     private String address;
     private int age;
     private String job;
@@ -39,4 +57,20 @@ public class Person {
     public void setJob(String job) {
         this.job = job;
     }
+
+    public Person(PersonRequestDto requestDto){
+        this.name = requestDto.getName();
+        this.address = requestDto.getAddress();
+        this.age = requestDto.getAge();
+        this.job = requestDto.getJob();
+    }
+
+    public void update(PersonRequestDto requestDto){
+        this.name = requestDto.getName();
+        this.address = requestDto.getAddress();
+        this.age = requestDto.getAge();
+        this.job = requestDto.getJob();
+    }
+
+
 }
