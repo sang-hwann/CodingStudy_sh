3f8199ddb883bf4fd12eb1fdc2ad70d74a7f8fbe
skyi0909@naver.com <skyi0909@naver.com>
2023-11-07 02:19:03 +0900

    Memo 코드

--- CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -1,6 +1,5 @@
 package com.sparta.week03.domain;
 
-import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,8 +10,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // entity가 자동으로 컬럼을 인식
 @EntityListeners(AuditingEntityListener.class)
-@Getter
-public abstract class Timestamped {
+public class Timestamped {
     @CreatedDate
     private LocalDateTime createdAt;
 
