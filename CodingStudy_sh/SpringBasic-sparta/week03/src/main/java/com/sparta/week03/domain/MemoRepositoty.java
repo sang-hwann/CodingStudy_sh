91133fa3ba56eecfae8c638a0314fe785969d502
skyi0909@naver.com <skyi0909@naver.com>
2023-11-09 03:54:35 +0900

    Memo 코드

--- CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/MemoRepositoty.java
+++ CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/MemoRepositoty.java
@@ -2,10 +2,8 @@ package com.sparta.week03.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepositoty extends JpaRepository<Memo, Long> {
-    //findAllByOrderByModifiedAtDesc 원래 이렇게인데 하루전~ 현시각까지로 수정
-    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start,LocalDateTime end);
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
