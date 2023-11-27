00498c02e4971b1123ef05de5a404c4b078175f4
skyi0909@naver.com <skyi0909@naver.com>
2023-11-28 02:20:32 +0900

    html 영역

--- CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/Week03Application.java
+++ CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/Week03Application.java
@@ -2,8 +2,10 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //수정발생시 자동으로 업데이트
 public class Week03Application {
 
 	public static void main(String[] args) {
--- CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
+++ CodingStudy_sh/SpringBasic-sparta/week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.week03.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // entity가 자동으로 컬럼을 인식
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class Timestamped {
     @CreatedDate
     private LocalDateTime createdAt;
