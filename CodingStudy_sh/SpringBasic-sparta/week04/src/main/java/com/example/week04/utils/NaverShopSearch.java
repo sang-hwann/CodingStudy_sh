5e1156fa2c51ecad5fabbe3453037f80288b6b57
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-12-10 15:42:13 +0900

    네이버API 사용하기위해 jsonobject 이용 코드 추가증 2

--- CodingStudy_sh/SpringBasic-sparta/week04/src/main/java/com/example/week04/utils/NaverShopSearch.java
+++ CodingStudy_sh/SpringBasic-sparta/week04/src/main/java/com/example/week04/utils/NaverShopSearch.java
@@ -1,9 +1,13 @@
 package com.example.week04.utils;
 
+import com.example.week04.models.ItemDto;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NaverShopSearch {
 
@@ -25,14 +29,33 @@ public class NaverShopSearch {
         return response;
     }
 
-    public static void main(String[] args) {
-        NaverShopSearch naverShopSearch = new NaverShopSearch();
-        String result = naverShopSearch.search("아이맥");
+    // 메소드로 구성해서 사용할수 있도록 구성
+    public List<ItemDto> fromJSONtoItems(String result){
         JSONObject rjson = new JSONObject(result); //result 문자열을 이야기한다. result이란 문자열을 JSONObject로 구성한다.
         JSONArray items = rjson.getJSONArray("items"); //검색한 rjson에서 items의 Json배열을 꺼내겠다는 의미
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
         for (int i = 0; i < items.length(); i++) {
             JSONObject itemJson = items.getJSONObject(i);
-            System.out.println(itemJson);
+//            System.out.println(itemJson); //밑의 방식으로 각 키값으로 값을 가져와 출력할수도 있다.
+//            String title = itemJson.getString("title");
+//            String image = itemJson.getString("image");
+//            int lprice = itemJson.getInt("lprice");
+//            String link= itemJson.getString("link");
+//            System.out.println(lprice);
+
+            ItemDto itemDto = new ItemDto(itemJson); // 그런식으로 JsonObject를 Dto에 값을 입력
+
+            itemDtoList.add(itemDto); // 받은 json값을 리스트에 추가
         }
+        return itemDtoList;
     }
+
+    //위의 메소드내용이 들어갔었는데 필요없어서 삭제함
+//    public static void main(String[] args) {
+//        NaverShopSearch naverShopSearch = new NaverShopSearch();
+//        String result = naverShopSearch.search("아이맥");
+//
+//    }
 }
